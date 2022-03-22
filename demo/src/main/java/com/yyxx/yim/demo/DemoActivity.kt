0b523db92ed40273bfb910a6ad538037ef726d0b
#Suyghur <Suyghurmjp@outlook.com>
2022-03-22 09:54:55 +0800

    项目初始化

---
+++ demo/src/main/java/com/yyxx/yim/demo/DemoActivity.kt
@@ -0,0 +1,15 @@
+package com.yyxx.yim.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author #Suyghur.
+ * Created on 2022/03/22
+ */
+class DemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
